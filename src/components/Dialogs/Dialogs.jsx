3d2a1070e2fc9e81a79af751c6a42b4e91b0cea2
Anton <brays777@mail.ru>
2020-02-17 05:45:35 +0300

    В Dialogs сделал рефакторинг

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,23 +1,37 @@
 import React from 'react';
 import classes from "./Dialogs.module.css";
+import {NavLink} from "react-router-dom";
 
+const DialogItem = (props) => {
+    return (
+        <div className={classes.dialog}>
+            <NavLink to={"/dialogs/" + props.id}>{props.name}</NavLink>
+        </div>
+    );
+}
+
+const Message = (props) => {
+    return (
+        <div className={classes.message}>{props.message}</div>
+    );
+}
 
 const Dialogs = (props) => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogItem}>
-                <div className={`${classes.dialog} ${classes.active}`}>Uchitel</div>
-                <div className={classes.dialog}>Telesic</div>
-                <div className={classes.dialog}>Andrusha</div>
-                <div className={classes.dialog}>SamiyUmniy</div>
-                <div className={classes.dialog}>Vladik</div>
-                <div className={classes.dialog}>Egorushka</div>
+                <DialogItem id = "1" name = "Uchitel"/>
+                <DialogItem id = "2" name = "Telesic"/>
+                <DialogItem id = "3" name = "Egorushka"/>
+                <DialogItem id = "4" name = "Andrusha"/>
+                <DialogItem id = "5" name = "Vladik"/>
+                <DialogItem id = "6" name = "SamiyUmniy"/>
             </div>
             <div className={classes.messages}>
-                <div className={classes.message}>Hi</div>
-                <div className={classes.message}>How are you?</div>
-                <div className={classes.message}>Hello</div>
-                <div className={classes.message}>Hey hey</div>
+                <Message id = "1" message="Hi"/>
+                <Message id = "2" message="How are you?"/>
+                <Message id = "3" message="Hello"/>
+                <Message id = "4" message="Yo Yo"/>
             </div>
             <div className={classes.block3}>block3</div>
         </div>
