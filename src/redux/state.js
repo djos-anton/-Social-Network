6a41ff11ab6f7a7bbc7cb70c95e66b93b2aea3a6
Anton <brays777@mail.ru>
2020-03-27 07:02:34 +0300

    create store in state.js и перенос всего содержимого state.js в объект store

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,7 +1,72 @@
-/*import {rerenderEntireTree} from "../render";*/
-let rerenderEntireTree = () => {
-    console.log('state changed');
+
+let store = {
+    _state : {
+        profilePage : {
+            postsData : [
+                {id : "1", message : "How? are you", counts : "21"},
+                {id : "2", message : "Hi you you", counts : "12"},
+                {id : "3", message : "Yo1 yo yo", counts : "121"},
+                {id : "4", message : "Yo2 you yo", counts : "1221"}
+            ],
+            newPostText: 'social_set'
+        },
+        messagesPage : {
+            dialogs : [
+                {id : "1", name : "UchGoda", img : "/img/ug250.png"},
+                {id : "2", name : "IvasShv", img : "/img/vas250.png"},
+                {id : "3", name : "EgoruBr", img : "/img/eg250.png"},
+                {id : "4", name : "AndruhK", img : "/img/and250.png"},
+                {id : "5", name : "VladMel", img : "/img/vl250.png"},
+                {id : "6", name : "VolodFo", img : "/img/vov250.png"}
+            ],
+            messages : [
+                {id : "1", message : "Hi"},
+                {id : "2", message : "How are you?"},
+                {id : "3", message : "Hello"},
+                {id : "4", message : "Yo Yo"}
+            ],
+            traMessages : [
+                {id : "1", trainingMessage : "training Hi"},
+                {id : "2", trainingMessage : "training Yo Yo"}
+            ]
+        },
+        sideBar : {
+            friend : [
+                {id : "6", name : "VolodFo", img : "/img/vov250.png"},
+                {id : "4", name : "AndruhK", img : "/img/and250.png"},
+                {id : "2", name : "IvasShv", img : "/img/vas250.png"}
+            ]
+        }
+    },
+    getState(){
+        return this._state;
+    },
+    _callSubscriber()  {
+        console.log('state changed');
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: /*postMessage*/ this._state.profilePage.newPostText,
+            counts: 7
+        };
+        this._state.profilePage.postsData.push(newPost);
+        this._state.profilePage.newPostText = "";
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    }
 }
+
+/*import {rerenderEntireTree} from "../render";*/
+/*let rerenderEntireTree = () => {
+    console.log('state changed');
+}*/
 
 /*import React from "react";*/
 /*let postsData = [
@@ -32,7 +97,7 @@ let messages = [
         {id : "2", name : "IvasShv", img : "/img/vas250.png"}
     ];*/
 
-let state = {
+/*let state = {
     profilePage : {
         postsData : [
             {id : "1", message : "How? are you", counts : "21"},
@@ -69,28 +134,29 @@ let state = {
             {id : "2", name : "IvasShv", img : "/img/vas250.png"}
         ]
     }
-}
+}*/
 
-window.state = state;
 
-export const addPost = (/*postMessage*/) => {
+
+/*export const addPost = (/!*postMessage*!/) => {
     let newPost = {
       id: 5,
-      message: /*postMessage*/ state.profilePage.newPostText,
+      message: /!*postMessage*!/ state.profilePage.newPostText,
       counts: 7
     };
     state.profilePage.postsData.push(newPost);
     state.profilePage.newPostText = "";
     rerenderEntireTree(state);
-}
+}*/
 
-export const updateNewPostText = (newText) => {
+/*export const updateNewPostText = (newText) => {
     state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
-}
+}*/
 
-export const subscribe = (observer) => {
+/*export const subscribe = (observer) => {
     rerenderEntireTree = observer;
-}
+}*/
 
-export default state;
+export default store;
+window.store = store;
