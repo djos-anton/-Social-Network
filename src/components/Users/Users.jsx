2f43ca6c510eb302c8ab55b8ad2b07729c1b515f
Anton <brays777@mail.ru>
2020-05-04 20:02:31 +0300

    В Users.jsx подключил сервер API, исправил не состыковки своих значений и тех, что приходят с сервера, внутри функции Users создал ещё одну функцию getUsers, для того, чтобы не было side Effecta_51_52

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,38 +1,73 @@
 import React from 'react';
 import classes from "./Users.module.css";
+import * as axios from "axios";
+import userPhoto from "../../assets/images/unnamed.png";
 
 const Users = (props) => {
 
-    if(props.users.length === 0){
-    props.setUsers(
-        [
-            {id : "1", img: "/img/olga.png", followed : false, nameLastName : "ValiaOvch", university : "МГУ", location: {town : "Севастополь", street : "Зуйская, 15"}},
-            {id : "2", img: "/img/olga.png", followed : true, nameLastName : "KsushaTih", university : "ПрибороТракторный", location: {town : "Керчь", street : "Пушкина, 33"}},
-            {id : "3", img: "/img/olga.png", followed : true, nameLastName : "NastiaKork", university : "Юридический", location: {town : "Симферополь", street : "Киевская, 24"}},
-            {id : "4", img: "/img/olga.png", followed : false, nameLastName : "OlgaChig", university : "ТНУ", location: {town : "Бахчисарай", street : "Ленина, 25"}}
-        ]
-    );
-    }
+    let getUsers = () => {
 
+        if (props.users.length === 0) {
+
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+
+                props.setUsers(response.data.items);
+            });
+
+            /*props.setUsers(
+                [
+                    {id : "1",
+                        img: "/img/olga.png",
+                        followed : false,
+                        nameLastName : "ValiaOvch",
+                        university : "МГУ",
+                        location: {town : "Севастополь", street : "Зуйская, 15"}},
+                    {id : "2",
+                        img: "/img/olga.png",
+                        followed : true,
+                        nameLastName : "KsushaTih",
+                        university : "ПрибороТракторный",
+                        location: {town : "Керчь", street : "Пушкина, 33"}},
+                    {id : "3",
+                        img: "/img/olga.png",
+                        followed : true,
+                        nameLastName : "NastiaKork",
+                        university : "Юридический",
+                        location: {town : "Симферополь", street : "Киевская, 24"}},
+                    {id : "4",
+                        img: "/img/olga.png",
+                        followed : false,
+                        nameLastName : "OlgaChig",
+                        university : "ТНУ",
+                        location: {town : "Бахчисарай", street : "Ленина, 25"}}
+                ]
+            );*/
+        }
+    }
     return (
         <div className={classes.users}>
             <div className={classes.people}>
+                <button onClick={getUsers}>GET USERS</button>
                 {
-                    props.users.map(u=> <div key = {u.id}>
+                    props.users.map(u => <div key={u.id}>
                         <div className={classes.peopleAvatar}>
-                            <img src={u.img}/>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto}/>
                         </div>
                         <div className={classes.peopleData}>
-                            <div>{u.nameLastName}</div>
-                            <div>{u.university}</div>
-                            <div>{u.location.town}</div>
-                            <div>{u.location.street}</div>
+                            <div>{u.name}</div>
+                            <div>{u.status}</div>
+                            <div>{"u.location.town"}</div>
+                            <div>{"u.location.street"}</div>
                             <br/>
                         </div>
                         <div className={classes.peopleButton}>
                             {u.followed
-                                ? <button className={classes.buttonAdd} onClick={()=>{props.unfollow(u.id)}}>Добавить в друзья</button>
-                                : <button className={classes.buttonRemove} onClick={()=>{props.follow(u.id)}}>Удалить из друзей</button>}
+                                ? <button className={classes.buttonAdd} onClick={() => {
+                                    props.unfollow(u.id)
+                                }}>Добавить в друзья</button>
+                                : <button className={classes.buttonRemove} onClick={() => {
+                                    props.follow(u.id)
+                                }}>Удалить из друзей</button>}
 
                         </div>
                     </div>)
